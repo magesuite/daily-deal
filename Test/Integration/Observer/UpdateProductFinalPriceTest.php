059cf7962ca03cf8816363bcad2a3bf89c5511aa
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2023-01-19 07:07:38 +0000

    fix: [DOG-1462] fix integration tests - compatibility magento 245

--- Test/Integration/Observer/UpdateProductFinalPriceTest.php
+++ Test/Integration/Observer/UpdateProductFinalPriceTest.php
@@ -7,6 +7,7 @@ class UpdateProductFinalPriceTest extends AbstractUpdateProductFinalPrice
     /**
      * @magentoDataFixture MageSuite_DailyDealCommerce::Test/Integration/_files/simple_product_with_dd.php
      * @magentoConfigFixture current_store daily_deal/general/active 1
+     * @magentoConfigFixture current_store catalog/price/scope 1
      * @magentoAppIsolation enabled
      * @magentoDbIsolation enabled
      * @magentoAppArea frontend
