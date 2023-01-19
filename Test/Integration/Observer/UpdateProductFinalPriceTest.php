6698c51dfc45686c86e9a7acef0c833517efe928
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2023-01-19 09:11:12 +0000

    Update UpdateProductFinalPriceTest.php

--- Test/Integration/Observer/UpdateProductFinalPriceTest.php
+++ Test/Integration/Observer/UpdateProductFinalPriceTest.php
@@ -5,7 +5,7 @@ namespace MageSuite\DailyDeal\Test\Integration\Observer;
 class UpdateProductFinalPriceTest extends AbstractUpdateProductFinalPrice
 {
     /**
-     * @magentoDataFixture MageSuite_DailyDealCommerce::Test/Integration/_files/simple_product_with_dd.php
+     * @magentoDataFixture MageSuite_DailyDeal::Test/Integration/_files/simple_product_with_dd.php
      * @magentoConfigFixture current_store daily_deal/general/active 1
      * @magentoConfigFixture current_store catalog/price/scope 1
      * @magentoAppIsolation enabled
