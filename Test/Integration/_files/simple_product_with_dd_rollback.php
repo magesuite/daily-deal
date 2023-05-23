934f6e2629bf13a4597b229dc238bda4ea552ff8
pre-commit fix Piotr Matras <piotr.matras@creativestyle.pl>
2023-05-23 08:06:09 +0000

    auto fixes from pre-commit CI job
    job url: https://gitlab.creativestyle.pl/magesuite/daily-deal/-/jobs/460912

--- Test/Integration/_files/simple_product_with_dd_rollback.php
+++ Test/Integration/_files/simple_product_with_dd_rollback.php
@@ -4,4 +4,3 @@ declare(strict_types=1);
 
 \Magento\TestFramework\Workaround\Override\Fixture\Resolver::getInstance()
     ->requireDataFixture('Magento/Catalog/_files/products_new_rollback.php');
-
